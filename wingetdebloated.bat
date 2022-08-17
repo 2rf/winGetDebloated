ff11dc3bf3a757e3e302f09f04c2324bdc8f7f13
rf <88802582+2rf@users.noreply.github.com>
2022-08-18 01:50:15 +0200

    Update wingetdebloated.bat
    Literally everything is better

--- wingetdebloated.bat
+++ wingetdebloated.bat
@@ -1,97 +1,98 @@
 @echo off
-set /p restore="Incase you change your mind, it's recommended to make a system restore point. Open restore point manager? y/n: "
-if /i "%restore%" == "y" echo Click on "create" && start SystemPropertiesProtection.exe
-set /p edge="Do you want to uninstall edge? y/n: "
+cls & echo wingetDebloated - A winget bloatware removal tool made in batch & echo.
+
+echo Do you want to keep Microsoft Edge? y/n 
+set /p edge="> "
+echo Do you want to keep the Camera App? y/n
+set /p camera="> "
+
 set /p enter="Press ENTER twice to start debloating"
-set /p enter2="Press ENTER one more time to start debloating!"
-cls
-color 06 && echo Ignore errors, when a User-Account-Control popup occurs, click "Yes"
-@timeout 2 /nobreak
-cls && color 
+set /p enter2="Press ENTER one more time to start debloating"
 
-echo Starting to debloat...
-echo Uninstalling Cortana...
+
+cls & echo wingetDebloated - A winget bloatware removal tool made in batch & echo.
+
+
 winget uninstall cortana
-
-echo Uninstalling Skype...
 winget uninstall skype
 
-if /i "%EDGE%" == "y" echo Killing Edge tasks and uninstalling Edge... (NOTE: IF ERRORS OCCUR CLOSE THEM, if it's stuck on loading relaunch the script.) && taskkill /im msedge.exe && winget uninstall Microsoft.Edge
-if /i "%EDGE%" == "n" echo Skipping Edge
+if /i "%EDGE%" == "n" taskkill /f /im msedge.exe & winget uninstall edge --accept-source-agreements --silent 
+if /i %CAMERA% == "n" winget uninstall 9WZDNCRFJBBG --accept-source-agreements --silent
 
-echo Uninstalling Xbox Bloat...
-winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe
-winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe
-winget uninstall Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe
-winget uninstall Microsoft.XboxIdentityProvider_8wekyb3d8bbwe
-winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe
-winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe
+: Xbox Applications
+winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxIdentityProvider_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling Groove-Music...
-winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe
+: Groove Music
+winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling FeedBack-Hub...
-winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
+: Feedback Hub
+winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling Microsoft-Tips...
-winget uninstall Microsoft.Getstarted_8wekyb3d8bbwe
+: Microsoft-Tips...
+winget uninstall Microsoft.Getstarted_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling 3D-Viewer...
-winget uninstall Microsoft.3DViewer_8wekyb3d8bbwe
-winget uninstall Microsoft.Microsoft3DViewer_8wekyb3d8bbwe
+: 3D Viewer
+winget uninstall 9NBLGGH42THS --accept-source-agreements --silent
 
-echo Uninstalling Microsoft Solitaire Collection
-winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
+: MS Solitaire
+winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling Paint-3D...
-winget uninstall Microsoft.MSPaint_8wekyb3d8bbwe
+: Paint-3D...
+winget uninstall 9NBLGGH5FV99 --accept-source-agreements --silent
 
-echo Uninstalling MSN-Weather also known as "Weather" App...
-winget uninstall Microsoft.BingWeather_8wekyb3d8bbwe
+: Weather 
+winget uninstall Microsoft.BingWeather_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling Mail / Calendar...
-winget uninstall microsoft.windowscommunicationsapps_8wekyb3d8bbwe
+: Mail / Calendar
+winget uninstall microsoft.windowscommunicationsapps_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling Your Phone...
-winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe
+:Your Phone
+winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe --accept-source-agreements --silent
 
-echo Uninstalling Microsoft Contacts...
+: People
 winget uninstall Microsoft.People_8wekyb3d8bbwe
 
-echo Uninstalling Microsoft.Pay/Wallet...
+: MS Pay 
 winget uninstall Microsoft.Wallet_8wekyb3d8bbwe
 
-echo Uninstalling Microsoft Maps...
+: MS Maps
 winget uninstall Microsoft.WindowsMaps_8wekyb3d8bbwe
 
-echo Uninstalling OneNote...
+: OneNote
 winget uninstall Microsoft.Office.OneNote_8wekyb3d8bbwe
 
-echo Uninstalling Office...
+: MS Office
 winget uninstall Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
 
-echo Uninstalling Voice Recorder...
+: Voice Recorder
 winget uninstall Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe
 
-echo Uninstalling Movies and TV...
+: Movies & TV
 winget uninstall Microsoft.ZuneVideo_8wekyb3d8bbwe
 
-echo Uninstalling Mixed Reality-Portal...
+: Mixed Reality-Portal
 winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe
 
-echo Uninstalling Sticky Notes...
+: Sticky Notes...
 winget uninstall Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe
 
-echo Uninstalling Get Help...
+: Get Help
 winget uninstall Microsoft.GetHelp_8wekyb3d8bbwe
 
-echo Uninstalling OneDrive...
+:OneDrive
 winget uninstall Microsoft.OneDrive
 
+: Windows 11 Bloatware
 winget uninstall Microsoft.Todos_8wekyb3d8bbwe
 winget uninstall Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe
 winget uninstall Microsoft.BingNews_8wekyb3d8bbwe
 winget uninstall MicrosoftTeams_8wekyb3d8bbwe
+winget uninstall disney+ 
 
-echo Done! If you want to reinstall an app simply look it up on the Microsoft Store or Winget. Recover from system restore point if you decided to change your mind
+echo. & echo Done. Thank you for using this tool.
 pause
